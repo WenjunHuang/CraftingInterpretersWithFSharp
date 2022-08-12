3777c706877802af1282e68de38c068ae77125f1
WenjunHuang <wenjun.huang80@gmail.com>
2022-08-12 18:42:56 +0800

    Initial commit

---
+++ lexer/Scanner.fs
@@ -0,0 +1,43 @@
+﻿namespace com.github.wenjunhuang.lox.lexer
+
+type Scanner(source: string) =
+    let mutable start = 0
+    let mutable current = 0
+    let mutable line = 1
+
+    let tokens: Token list = []
+    
+    let advance () =
+        current <- current + 1
+        source.[current - 1]
+
+    let isAtEnd () =
+        current >= source.Length
+        
+    let matchChar c =
+        if isAtEnd() then false
+        else if source.[current] == c then false
+        else
+            advance() |> ignore
+            true
+        
+    member this.scanTokens() =
+        let c = advance ()
+        match c with
+        | '(' -> this.addToken TokenType.LEFT_PAREN
+        | ')' -> this.addToken TokenType.RIGHT_PAREN
+        | '{' -> this.addToken TokenType.LEFT_BRACE
+        | '}' -> this.addToken TokenType.RIGHT_BRACE
+        | ',' -> this.addToken TokenType.COMMA
+        | '.' -> this.addToken TokenType.DOT
+        | '-' -> this.addToken TokenType.MINUS
+        | '+' -> this.addToken TokenType.PLUS
+        | ';' -> this.addToken TokenType.SEMICOLON
+        | '*' -> this.addToken TokenType.STAR
+        | '!' -> if (matchChar '=') then this.addToken TokenType.BANG_EQUAL else this.addToken TokenType.BANG
+        | _ -> failwith "todo"
+
+        
+    member this.addToken (tokenType:TokenType) (?literal: obj) =
+        ()
+        
---
+++ lexer/Token.fs
@@ -0,0 +1,7 @@
+﻿namespace com.github.wenjunhuang.lox.lexer
+
+type Token =
+    { tokenType: TokenType
+      lexeme: string
+      literal: option<obj>
+      line: int }
